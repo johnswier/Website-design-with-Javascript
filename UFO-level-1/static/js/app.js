458db0dc1188b7ef93c16bcb3f2fc389aab45292
John S <jswierc1@uncc.edu>
2020-09-29 18:04:01 -0400

    code for generating table data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,21 @@
-// from data.js
+// creating tabledata object from raw data file
 var tableData = data;
 
-// YOUR CODE HERE!
+// print to console to verify accuracy
+// console.log(tableData)
+
+// create a variable to hold table body HTML element
+var tbody = d3.select("tbody");
+
+// create and add table row into tbody for each object iterated on
+tableData.forEach(function (ufo) {
+    var row = tbody.append("tr");
+    
+    Object.entries(ufo).forEach(function ([key,value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
 
 

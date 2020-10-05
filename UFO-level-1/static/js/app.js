f6d90f892174f35e93d16d032329c0e164a0b7fb
John S <jswierc1@uncc.edu>
2020-10-05 09:55:00 -0400

    final script for level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,7 +2,7 @@
 var tableData = data;
 
 // print to console to verify accuracy
-// console.log(tableData)
+console.log(tableData)
 
 // create a variable to hold table body HTML element
 var tbody = d3.select("tbody");
@@ -17,5 +17,27 @@ tableData.forEach(function (ufo) {
     });
 });
 
+// create variable to hold input text
+var text = d3.select("#datetime");
 
 
+// create function to handle text input
+
+function handleChange(event) {
+    
+    d3.event.preventDefault();
+    tbody.html('');
+    var inputDate = d3.event.target.value;
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputDate);
+    filteredData.forEach(function (ufo) {
+        var row = tbody.append("tr");
+        
+        Object.entries(ufo).forEach(function ([key,value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+};
+
+text.on("change", handleChange);
